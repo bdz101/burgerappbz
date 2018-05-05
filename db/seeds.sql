3c43f69a36aa6139af459a782b24bd9c3ef990da
Brian Zeuch <bz901610@ohio.edu>
2018-05-05 10:31:38 -0400

    one

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--write insert queries to populate the burgers table with at least three entries
+USE burgers_db;
+
+INSERT INTO products (burger_name) VALUES ('Cheeseburger');
+INSERT INTO products (burger_name) VALUES ('Hamburger');
+INSERT INTO products (burger_name VALUES ('Bacon Burger');
